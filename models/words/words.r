6a6d41a09e43ab0f0b8abcd4a733163e70c17044
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-29 16:48:45 -0300

    env

--- models/words/words.r
+++ models/words/words.r
@@ -1,24 +1,23 @@
-file.path(
-"models",
-"words",
-"words.csv"
-) |> 
-read.csv() -> 
-words
+Sys.getenv("PTBRFN_MODELS") |>
+  file.path(
+    "words",
+    "words.csv"
+  ) |>
+  read.csv() -> words
 
-colMeans(words) |> 
-rbind(
-colMeans(
-words - words[,1]
-)
-) |> 
-round(2) |> 
-write.csv(
-file = file.path(
-"models",
-"words",
-"means.csv"
-),
-row.names = F,
-quote = F
-)
+colMeans(words) |>
+  rbind(
+    colMeans(
+      words - words[, 1]
+    )
+  ) |>
+  round(2) |>
+  write.csv(
+    file = file.path(
+      "models",
+      "words",
+      "means.csv"
+    ),
+    row.names = F,
+    quote = F
+  )
